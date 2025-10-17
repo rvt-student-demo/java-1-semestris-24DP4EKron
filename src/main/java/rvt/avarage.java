5345db116f51dc029d231af0a8fbf8d670b4f750
24DP4EKron <A242585EK@rvt.lv>
2025-10-17 12:08:42 +0300

    avarage 3 numbers

---
+++ src/main/java/rvt/avarage.java
@@ -0,0 +1,19 @@
+package rvt;
+import java.util.Scanner;
+
+public class avarage {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the third number:");
+        int third = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Videjais skaitlis ir" +
+            (first + second + third) / 3);
+
+    }
+}
